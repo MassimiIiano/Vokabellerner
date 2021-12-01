4cc8651ae305992b0b1a328ae6a7999157ee1155
eMTyMe <max02eu@gmail.com>
2021-12-01 10:21:43 +0100

    boi

--- src/net/tfobz/vokabeltrainer/dialogs/SettingsDialog.java
+++ src/net/tfobz/vokabeltrainer/dialogs/SettingsDialog.java
@@ -11,78 +11,57 @@ import javax.swing.JDialog;
 import javax.swing.JButton;
 import javax.swing.DefaultComboBoxModel;
 import java.awt.Font;
+import java.awt.Toolkit;
 
-public class SettingsDialog {
+public class SettingsDialog
+{
 
-    private JDialog frmEinstellungen;
+	private JDialog frmEinstellungen;
 
-    /**
-     * Launch the application.
-     */
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    SettingsDialog window = new SettingsDialog();
-                    window.frmEinstellungen.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	public SettingsDialog() {
+		frmEinstellungen = new JDialog();
 
-    /**
-     * Create the application.
-     */
-    public SettingsDialog() {
-        initialize();
-    }
+		frmEinstellungen.setTitle("Einstellungen");
+		frmEinstellungen.setResizable(false);
+		frmEinstellungen.setBounds((Toolkit.getDefaultToolkit().getScreenSize().width/2)-250, (Toolkit.getDefaultToolkit().getScreenSize().height/2)-125, 500, 250);
+		frmEinstellungen.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		frmEinstellungen.getContentPane().setLayout(null);
 
-    /**
-     * Initialize the contents of the frame.
-     */
-    private void initialize() {
-        frmEinstellungen = new JDialog();
+		JCheckBox chckbxGrosskleinschreibungBeachten = new JCheckBox("Gross-/Kleinschreibung beachten");
+		chckbxGrosskleinschreibungBeachten.setBounds(8, 9, 466, 25);
+		frmEinstellungen.getContentPane().add(chckbxGrosskleinschreibungBeachten);
 
-        frmEinstellungen.setTitle("Einstellungen");
-        frmEinstellungen.setResizable(false);
-        frmEinstellungen.setBounds(100, 100, 500, 250);
-        frmEinstellungen.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-        frmEinstellungen.getContentPane().setLayout(null);
+		JCheckBox chckbxNurLektionenMit = new JCheckBox("Nur Lektionen mit abgelaufener Errinerung");
+		chckbxNurLektionenMit.setBounds(8, 39, 466, 25);
+		frmEinstellungen.getContentPane().add(chckbxNurLektionenMit);
 
-        JCheckBox chckbxGrosskleinschreibungBeachten = new JCheckBox("Gross-/Kleinschreibung beachten");
-        chckbxGrosskleinschreibungBeachten.setBounds(8, 9, 466, 25);
-        frmEinstellungen.getContentPane().add(chckbxGrosskleinschreibungBeachten);
+		JLabel lblTheme = new JLabel("Theme:");
+		lblTheme.setBounds(12, 83, 56, 16);
+		frmEinstellungen.getContentPane().add(lblTheme);
 
-        JCheckBox chckbxNurLektionenMit = new JCheckBox("Nur Lektionen mit abgelaufener Errinerung");
-        chckbxNurLektionenMit.setBounds(8, 39, 466, 25);
-        frmEinstellungen.getContentPane().add(chckbxNurLektionenMit);
+		JComboBox<String> comboBox = new JComboBox<String>();
+		comboBox.setModel(new DefaultComboBoxModel<String>(new String[] { "Dark", "White" }));
+		comboBox.setBounds(80, 80, 108, 22);
+		frmEinstellungen.getContentPane().add(comboBox);
 
-        JLabel lblTheme = new JLabel("Theme:");
-        lblTheme.setBounds(12, 83, 56, 16);
-        frmEinstellungen.getContentPane().add(lblTheme);
+		JButton btnImport = new JButton("Import");
+		btnImport.setBounds(385, 177, 97, 25);
+		frmEinstellungen.getContentPane().add(btnImport);
 
-        JComboBox<String> comboBox = new JComboBox<String>();
-        comboBox.setModel(new DefaultComboBoxModel<String>(new String[] { "Dark", "White" }));
-        comboBox.setBounds(80, 80, 108, 22);
-        frmEinstellungen.getContentPane().add(comboBox);
+		JButton btnExport = new JButton("Export");
+		btnExport.setBounds(276, 177, 97, 25);
+		frmEinstellungen.getContentPane().add(btnExport);
 
-        JButton btnErrinerung = new JButton("Errinern");
-        
-        JButton btnImport = new JButton("Import");
-        btnImport.setBounds(385, 177, 97, 25);
-        frmEinstellungen.getContentPane().add(btnImport);
-
-        JButton btnExport = new JButton("Export");
-        btnExport.setBounds(276, 177, 97, 25);
-        frmEinstellungen.getContentPane().add(btnExport);
-
-        JButton btnAddLanguage = new JButton("Add Language");
-        btnAddLanguage.setFont(new Font("Tahoma", Font.BOLD, 13));
-        btnAddLanguage.setBounds(8, 177, 133, 25);
-        frmEinstellungen.getContentPane().add(btnAddLanguage);
-        JRootPane rootPane = SwingUtilities.getRootPane(btnAddLanguage);
-        rootPane.setDefaultButton(btnAddLanguage);
-    }
+		JButton btnAddLanguage = new JButton("Add Language");
+		btnAddLanguage.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnAddLanguage.setBounds(8, 177, 133, 25);
+		frmEinstellungen.getContentPane().add(btnAddLanguage);
+		JRootPane rootPane = SwingUtilities.getRootPane(btnAddLanguage);
+		rootPane.setDefaultButton(btnAddLanguage);
+		
+		frmEinstellungen.setVisible(true);
+	}
 }
