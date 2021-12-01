b5853a9d7e22f2b0743ce88d497e6a79be6da514
MassimiIano <massimiliano.mola.bzs@gmail.com>
2021-12-01 08:55:01 +0100

    Finisched DateButton

--- src/net/tfobz/vokabeltrainer/DateButton.java
+++ src/net/tfobz/vokabeltrainer/DateButton.java
@@ -1,33 +1,49 @@
 package net.tfobz.vokabeltrainer;
 
 import java.awt.event.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.swing.*;
 import java.awt.*;
 
 public class DateButton extends JButton {
     JTextField dt;
+    Date date;
+    String dateFormat = "dd.MM.yyyy";
+
     public DateButton() {
         this.addActionListener(new ActionListener() {
             @Override
-			public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent e) {
                 getDateText();
-
-			}
-		});
+            }
+        });
     }
 
     public JTextField getDateText() {
         setVisible(false);
         dt = new JTextField();
         dt.setBounds(new Rectangle(new Point(getX(), getY()), getSize()));
+        dt.setVisible(true);
+        dt.setText("dd.mm.yyyy");
         dt.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                this;                
+                try {
+                    date = new SimpleDateFormat("dd.MM.yyyy").parse(dt.getText().strip());
+                    dt.setVisible(false);
+                    DateButton.this.setVisible(true);
+                } catch (ParseException e1) {
+                    dt.setText("Invalid date");
+                    return;
+                }
             }
         });
         return dt;
     }
 
-
+    public Date getDate() {
+        return date;
+    }
 }
