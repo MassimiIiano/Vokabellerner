04193833276bea62233b29fdbdaf4585255f84b6
eMTyMe <max02eu@gmail.com>
2021-12-10 20:37:53 +0100

    ErrinerungsDialog.java - GUI almost complete

--- src/net/tfobz/vokabeltrainer/dialogs/ChooseDate.java
+++ src/net/tfobz/vokabeltrainer/dialogs/ChooseDate.java
@@ -1,67 +1,33 @@
 package net.tfobz.vokabeltrainer.dialogs;
 
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.EventQueue;
-import java.awt.GridLayout;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.FlowLayout;
 import java.util.Calendar;
-import java.util.Properties;
 import javax.swing.*;
 
-import org.jdatepicker.impl.JDatePanelImpl;
 import org.jdatepicker.impl.JDatePickerImpl;
 import org.jdatepicker.impl.UtilDateModel;
 
 public class ChooseDate extends JPanel
 {
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				new ChooseDate();
-			}
-		});
-	}
 	
-	public ChooseDate() {
-		//super(parent);
-		setBounds(500, 500, 500, 50);
-		setLayout(new GridLayout(1, 2));
-			
-		UtilDateModel model = new UtilDateModel();
-		Properties p = new Properties();
-		Calendar calendar = Calendar.getInstance();
-		p.put("date.day", String.valueOf(calendar.get(Calendar.DATE)));
-		p.put("date.month", String.valueOf(calendar.get(Calendar.MONTH)));
-		p.put("date.year", String.valueOf(calendar.get(Calendar.YEAR)));
-		JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
-		JDatePickerImpl datePicker = new JDatePickerImpl(datePanel, new DateFormat());
-		
-		JButton btn_ok = new JButton("Ok");
-		btn_ok.addMouseListener(new MouseAdapter() {
-			
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				UtilDateModel test = (UtilDateModel) datePicker.getModel();
-			}
-			
-		});
-		/*JButton btn_cancel = new JButton("Abbrechen");
-		btn_cancel.addMouseListener(new MouseAdapter() {
-
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				dispose();
-			}
-		});
-		*/
+	JDatePickerImpl datePicker;
+	
+	public ChooseDate(JLabel l_date, JDatePickerImpl datePicker) {
+		this.datePicker = datePicker;
+		setLayout(new FlowLayout());
 		
 
-		
-		add(btn_ok);
+			
 		add(datePicker);
 		setVisible(true);
+	}
+	
+	public String getDateAsString() {
+		
+		UtilDateModel test = (UtilDateModel) datePicker.getModel();
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(test.getValue());
+		return String.valueOf(cal.get(Calendar.DATE)) + "-" +String.valueOf(cal.get(Calendar.MONTH)) + "-" +
+		String.valueOf(cal.get(Calendar.YEAR));
 	}
 }
--- src/net/tfobz/vokabeltrainer/dialogs/ErrinerungDialog.java
+++ src/net/tfobz/vokabeltrainer/dialogs/ErrinerungDialog.java
@@ -6,7 +6,6 @@ import org.jdatepicker.impl.JDatePanelImpl;
 import org.jdatepicker.impl.JDatePickerImpl;
 import org.jdatepicker.impl.UtilDateModel;
 
-import java.awt.CardLayout;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
@@ -19,8 +18,11 @@ import java.awt.event.MouseEvent;
 import java.util.Calendar;
 import java.util.Properties;
 
-public class ErrinerungDialog extends JFrame{
+public class ErrinerungDialog
+{
 
+	JFrame frame = new JFrame();
+	JLabel l_mitDatum;
 	/**
 	 * Launch the application.
 	 */
@@ -40,56 +42,150 @@ public class ErrinerungDialog extends JFrame{
 	 * Create the application.
 	 */
 	public ErrinerungDialog() {
-		setBounds((Toolkit.getDefaultToolkit().getScreenSize().width / 2) - 350,
-				(Toolkit.getDefaultToolkit().getScreenSize().height / 2) - 250, 700, 500);
-		setResizable(false);
-		setTitle("Errinerung einstellen");
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		getContentPane().setLayout(new CardLayout());
+		frame.setBounds((Toolkit.getDefaultToolkit().getScreenSize().width / 2) - 275,
+				(Toolkit.getDefaultToolkit().getScreenSize().height / 2) - 91, 550, 182);
+		frame.setResizable(false);
+		frame.setTitle("Errinerung einstellen");
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		FlowLayout layout = new FlowLayout();
+		frame.getContentPane().setLayout(layout);
+		layout.setAlignment(FlowLayout.TRAILING);
+		layout.setVgap(15);
+
+		UtilDateModel model = new UtilDateModel();
+		Properties p = new Properties();
+		Calendar calendar = Calendar.getInstance();
+		p.put("date.day", String.valueOf(calendar.get(Calendar.DATE)));
+		p.put("date.month", String.valueOf(calendar.get(Calendar.MONTH)));
+		p.put("date.year", String.valueOf(calendar.get(Calendar.YEAR)));
+		JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
+		JDatePickerImpl datePicker = new JDatePickerImpl(datePanel, new DateFormat());
+		
+		ChooseDate cal = new ChooseDate(l_mitDatum, datePicker);
 		
 		JPanel panelPeriodisch = new JPanel();
 		panelPeriodisch.setLayout(new FlowLayout());
-		Font basic = new Font("Tahoma", Font.PLAIN, 25);
+		Font basic = new Font("Tahoma", Font.PLAIN, 23);
 		JLabel l_title1 = new JLabel("Errinern alle:");
 		l_title1.setFont(basic);
 		JTextField tf_values[] = new JTextField[3];
-		
-		
+
 		JLabel l_labels[] = new JLabel[3];
 		l_labels[0] = new JLabel("Wochen");
 		l_labels[1] = new JLabel("Tage");
 		l_labels[2] = new JLabel("Stunden");
-		
-		
-		for(int i = 0; i < 3; i++) {
-			tf_values[i] = new JTextField("0");
-			tf_values[i].setPreferredSize(new Dimension(50,25));
+
+		for (int i = 0; i < 3; i++) {
+			tf_values[i] = new JTextField("");
+			tf_values[i].setPreferredSize(new Dimension(50, 25));
+			tf_values[i].addKeyListener(new TextFieldKeyHoerer());
 			l_labels[i].setFont(basic);
 			l_labels[i].setSize(20, 25);
 		}
-		
-		
+
 		panelPeriodisch.add(l_title1);
 		System.out.println(tf_values[0].getText());
-		for(int i = 0; i < 3; i++) {
+		for (int i = 0; i < 3; i++) {
 			panelPeriodisch.add(tf_values[i]);
 			panelPeriodisch.add(l_labels[i]);
 		}
+
+		l_mitDatum = new JLabel("");
 		
-		String datum = "dd-MM-yyyy";
-		JLabel l_mitDatum = new JLabel();
+		l_mitDatum.setText("Oder am dd-MM-yyyy errinern:");
+		l_mitDatum.setFont(basic);
+
+		JButton btn_ok = new JButton("Errinerung setzten");
+		btn_ok.addMouseListener(new MouseAdapter() {
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				final String calDatum = cal.getDateAsString();
+				l_mitDatum.setText("Oder am " + calDatum + " errinern:");
+			}
+		});
 		
-		getContentPane().add(panelPeriodisch);
-		getContentPane().add(new ChooseDate());
-		setVisible(true);
+		JButton btn_cancel = new JButton("Abbrechen");
+		btn_cancel.addMouseListener(new MouseAdapter() {
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				frame.dispose();
+			}
+		});
+		
+		frame.getContentPane().add(panelPeriodisch);
+		frame.getContentPane().add(l_mitDatum);
+		frame.getContentPane().add(cal);
+		frame.getContentPane().add(btn_ok);
+		frame.getContentPane().add(btn_cancel);
+		frame.setVisible(true);
 	}
 
-	private class TextFieldHoerer extends KeyAdapter {
-		
+	private class TextFieldKeyHoerer extends KeyAdapter
+	{
+
 		@Override
-		public void keyPressed(KeyEvent e) {
-			
+		public void keyReleased(KeyEvent e) {
+
+			JTextField tf = (JTextField) e.getSource();
+			char charBefore;
+			if (tf.getText().length() == 0) {
+
+				if (e.getKeyCode() >= KeyEvent.VK_0 && e.getKeyCode() <= KeyEvent.VK_9) {
+
+					synchronized (this) {
+						try {
+							wait(1);
+						} catch (InterruptedException e1) {
+						}
+					}
+					tf.setText(Character.toString(e.getKeyChar()));
+				} else
+
+					synchronized (this) {
+						try {
+							wait(1);
+						} catch (InterruptedException e1) {
+						}
+					}
+				tf.setText("");
+			} else if (tf.getText().length() == 1) {
+
+				charBefore = tf.getText().charAt(0);
+				if (e.getKeyCode() >= KeyEvent.VK_0 && e.getKeyCode() <= KeyEvent.VK_9) {
+
+					synchronized (this) {
+						try {
+							wait(1);
+						} catch (InterruptedException e1) {
+						}
+					}
+					tf.setText(charBefore + Character.toString(e.getKeyChar()));
+				} else
+					synchronized (this) {
+						try {
+							wait(1);
+						} catch (InterruptedException e1) {
+						}
+					}
+				tf.setText(charBefore + "");
+			} else if (tf.getText().length() >= 2) {
+
+				if (e.getKeyCode() != KeyEvent.VK_BACK_SPACE) {
+
+					String textBefore = tf.getText().substring(0, 2);
+					synchronized (this) {
+						try {
+							wait(100);
+						} catch (InterruptedException e1) {
+						}
+						tf.setText(textBefore);
+						System.out.println("");
+					}
+				}
+			}
 		}
 	}
-
 }
