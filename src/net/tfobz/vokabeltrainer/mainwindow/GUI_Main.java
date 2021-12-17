f8d1e7be2155dde2fd3871e1f7dd9438bccbfc0c
eMTyMe <max02eu@gmail.com>
2021-12-17 12:21:33 +0100

    fixed some stuff, writing out comments

--- src/net/tfobz/vokabeltrainer/mainwindow/GUI_Main.java
+++ src/net/tfobz/vokabeltrainer/mainwindow/GUI_Main.java
@@ -50,6 +50,8 @@ public class GUI_Main extends JFrame
 
 		// VokabeltrainerDB.loeschenLernkartei(0);
 		
+		//Zeigt Benutzer Auswahl aller gefundenen Lernkarteien, falls keine vorhanden ist, wird eine Starterkartei erstellt.
+		//Die Auswahl des Benutzer wird in einer Variable gespeichert.
 		try {
 			this.chosenKartei = ChooseKartei.chooseKartei(this);
 		} catch (NullPointerException e) {
@@ -60,6 +62,9 @@ public class GUI_Main extends JFrame
 
 			this.karteiNummer = this.chosenKartei.getNummer();
 
+			//Importiert die Karten der Lernkartei und gibt sie in das richtige Fach, falls keine angegeben wurde, werden die
+			//Karten in eine neues Fach gelegt. Falls bis zum Abschluss jenes Prozesses, weniger als 3 Fächer erstellt wurden,
+			//erstellt das Programm automatisch alle fehlenden bis zu insgesamt 3 Fächern.
 			importKarten();
 
 			JButton btnAddTab = new JButton("+");
@@ -70,143 +75,49 @@ public class GUI_Main extends JFrame
 			btnAddTab.setBackground(new Color(238, 238, 238));
 
 			this.tabbedPane.setBounds(0, 0, 744, 346);
-			for (int i = 0; i < 3; i++) {
+			
+			//Erstell die anfänglichen Tabs und weist ihnen die Zuhörer zu, je nachdem wie viele Fächer importiert wurden; mind. 3
+			for (int i = 0; i < VokabeltrainerDB.getFaecher(this.chosenKartei.getNummer()).size(); i++) {
 				Tab tab = new Tab(this.chosenKartei, this.tabbedPane);
-				tab.btnMinus.addMouseListener(new MouseAdapter() {
-
-					@SuppressWarnings("static-access")
-					@Override
-					public void mouseClicked(MouseEvent e) {
-
-						if (e.getButton() == 1) {
-							int decision = new JOptionPane().showConfirmDialog(GUI_Main.this,
-									"Möchten sie dieses Wort aus der Liste entfernen?", "Wort Löschen", JOptionPane.YES_NO_OPTION);
-							if (decision == JOptionPane.YES_OPTION) {
-
-								VokabeltrainerDB.loeschenKarte(GUI_Main.this.currentKarte.get(GUI_Main.this.selectedTab).getNummer());
-								updateCenterDialog(Color.BLACK, "Geben Sie Ihre Antwort ein: ");
-								createRandomKarte("In diesem Fach sind keine weiteren Wörter");
-								GUI_Main.this.tabs.get(GUI_Main.this.tabbedPane.getSelectedIndex()).eingabe.setText("");
-
-							} else {
-								return;
-							}
-						}
-					}
-				});
-
-				tab.btnPlus.addMouseListener(new MouseAdapter() {
-
-					@Override
-					public void mouseClicked(MouseEvent e) {
-
-						if (e.getButton() == 1) {
-							NewWordDialog newWord = new NewWordDialog();
-							newWord.labels[0].setText(GUI_Main.this.chosenKartei.getWortEinsBeschreibung());
-							newWord.labels[1].setText(GUI_Main.this.chosenKartei.getWortZweiBeschreibung());
-							newWord.btn_ok.addMouseListener(new MouseAdapter() {
-
-								@Override
-								public void mouseClicked(MouseEvent e) {
-
-									if (e.getButton() == 1) {
-										if (!newWord.textFields[0].getText().isEmpty() && !newWord.textFields[1].getText().isEmpty()) {
-											Karte k = new Karte();
-											k.setWortEins(newWord.textFields[0].getText());
-											k.setWortZwei(newWord.textFields[1].getText());
-											int check = VokabeltrainerDB.hinzufuegenKarte(GUI_Main.this.chosenKartei.getNummer(), k);
-											if (check == 0) {
-												JOptionPane.showMessageDialog(GUI_Main.this, "Wort erfolgreich hinzugefügt", "Neues Wort",
-														JOptionPane.INFORMATION_MESSAGE);
-												newWord.dispose();
-											} else {
-												JOptionPane.showMessageDialog(newWord, "Ein Fehler ist aufgetreten!", "Fehler bei hinzufügen",
-														JOptionPane.ERROR_MESSAGE);
-											}
-										} else {
-											JOptionPane.showMessageDialog(newWord, "Keine Wörter eingegeben!", "Fehler bei Eingabe",
-													JOptionPane.ERROR_MESSAGE);
-										}
-									}
-
-								}
-							});
-							newWord.btn_cancel.addMouseListener(new MouseAdapter() {
-
-								@Override
-								public void mouseClicked(MouseEvent e) {
-									newWord.dispose();
-								}
-
-							});
-						}
-
-					}
-				});
-
-				tab.eingabe.addKeyListener(new KeyAdapter() {
-
-					@Override
-					public void keyPressed(KeyEvent e) {
-
-						if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-							checkEntry();
-						}
-					}
-
-				});
-
-				tab.btnOk.addMouseListener(new MouseAdapter() {
-
-					@Override
-					public void mouseClicked(MouseEvent e) {
-
-						if (e.getButton() == 1) {
-							checkEntry();
-						}
-
-					}
-				});
-
-				tab.btnSwitch.addMouseListener(new MouseAdapter() {
-
-					@Override
-					public void mouseClicked(MouseEvent e) {
-
-						if (e.getButton() == 1) {
-							VokabeltrainerDB.exportierenKarten(GUI_Main.this.chosenKartei.getNummer(),
-									"Lernkarteien/" + GUI_Main.this.chosenKartei.getBeschreibung() + ".txt", true);
-							VokabeltrainerDB.aendernLernkartei(new Lernkartei(GUI_Main.this.chosenKartei.getNummer(),
-									GUI_Main.this.chosenKartei.getBeschreibung(), GUI_Main.this.chosenKartei.getWortEinsBeschreibung(),
-									GUI_Main.this.chosenKartei.getWortZweiBeschreibung(), !GUI_Main.this.chosenKartei.getRichtung(),
-									GUI_Main.this.chosenKartei.getGrossKleinschreibung()));
-							VokabeltrainerDB.loeschenAlleFaecher(GUI_Main.this.chosenKartei.getNummer());
-							importKarten();
-							updateCenterDialog(Color.BLACK, "Geben Sie Ihre Antwort ein: ");
-							createRandomKarte("In diesem Fach sind keine weiteren Wörter");
-							GUI_Main.this.tabs.get(GUI_Main.this.tabbedPane.getSelectedIndex()).eingabe.setText("");
-						}
-
-					}
-				});
+				
+				addTabListener(tab);
+				
 				this.tabs.add(tab);
 				this.tabbedPane.addTab("Fach " + (i + 1), this.tabs.get(i));
 			}
 
+			//Der TabbedPane und dem '+'-Knopf neben den Tabs (graphisch) werden Maushörer zugewiesen 
 			this.tabbedPane.addMouseListener(new MouseAdapter() {
 
 				@Override
 				public void mousePressed(MouseEvent e) {
+					//Falls Mausrad-Knopf, soll Tab gelöscht werden
 					if (e.getButton() == 2) {
+						//Damit diese Löschen-Funktion nicht Tab wechselt sobald das Mausrad gedrückt wird, wird auf den
+						//letzten aufgerufenen Tab gewechselt
 						GUI_Main.this.tabbedPane.setSelectedIndex(GUI_Main.this.selectedTab);
+						
+						//Überprüft ob innerhalb der Fläche des letzten Tabs gedrückt wurde 
 						int left = 58 * (GUI_Main.this.tabbedPane.getTabCount() - 1);
 						int right = 58 * (GUI_Main.this.tabbedPane.getTabCount() - 1) + 58;
 						if (e.getX() >= left && e.getX() <= right && e.getY() > 0 && e.getY() < 20) {
+			
+							//Entfernt das Fach aus der Datenbank und der TabbedPane und löscht die currentKarte dieses Faches
+							//("Dieses Faches" ist relativ, da immer nur das letzte Fach gelöscht werden kann)
 							int fachNummer = GUI_Main.this.tabbedPane.getTabCount();
 							VokabeltrainerDB.loeschenFach(GUI_Main.this.karteiNummer, fachNummer);
 							GUI_Main.this.tabbedPane.remove(GUI_Main.this.tabbedPane.getTabCount() - 1);
 							GUI_Main.this.currentKarte.remove(GUI_Main.this.currentKarte.size()-1);
+							
+							//Setzt den Knopf um Tabs hinzuzufügen immer hinter den letzten Tab
 							btnAddTab.setLocation(btnAddTab.getX() - 58, 0);
+							
+							//Probiert aus ob der Index noch stimmt, falls nicht wird er um 1 verringert
+							try {
+								GUI_Main.this.tabbedPane.setSelectedIndex(GUI_Main.this.selectedTab);
+							}catch(IndexOutOfBoundsException e1) {
+								GUI_Main.this.selectedTab--;
+							}
 						}
 					}
 				}
@@ -319,7 +230,7 @@ public class GUI_Main extends JFrame
 									+ GUI_Main.this.currentKarte.get(GUI_Main.this.selectedTab).getWortZwei() + "'");
 							synchronized (this) {
 								try {
-									this.wait(2500);
+									this.wait(2000);
 								} catch (InterruptedException e) {
 									e.printStackTrace();
 								}
@@ -369,4 +280,125 @@ public class GUI_Main extends JFrame
 		}
 	}
 
+	public void addTabListener(Tab tab) {
+		tab.btnMinus.addMouseListener(new MouseAdapter() {
+
+			@SuppressWarnings("static-access")
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				if (e.getButton() == 1) {
+					int decision = new JOptionPane().showConfirmDialog(GUI_Main.this,
+							"Möchten sie dieses Wort aus der Liste entfernen?", "Wort Löschen", JOptionPane.YES_NO_OPTION);
+					if (decision == JOptionPane.YES_OPTION) {
+
+						VokabeltrainerDB.loeschenKarte(GUI_Main.this.currentKarte.get(GUI_Main.this.selectedTab).getNummer());
+						updateCenterDialog(Color.BLACK, "Geben Sie Ihre Antwort ein: ");
+						createRandomKarte("In diesem Fach sind keine weiteren Wörter");
+						GUI_Main.this.tabs.get(GUI_Main.this.tabbedPane.getSelectedIndex()).eingabe.setText("");
+
+					} else {
+						return;
+					}
+				}
+			}
+		});
+
+		tab.btnPlus.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				if (e.getButton() == 1) {
+					NewWordDialog newWord = new NewWordDialog();
+					newWord.labels[0].setText(GUI_Main.this.chosenKartei.getWortEinsBeschreibung());
+					newWord.labels[1].setText(GUI_Main.this.chosenKartei.getWortZweiBeschreibung());
+					newWord.btn_ok.addMouseListener(new MouseAdapter() {
+
+						@Override
+						public void mouseClicked(MouseEvent e) {
+
+							if (e.getButton() == 1) {
+								if (!newWord.textFields[0].getText().isEmpty() && !newWord.textFields[1].getText().isEmpty()) {
+									Karte k = new Karte();
+									k.setWortEins(newWord.textFields[0].getText());
+									k.setWortZwei(newWord.textFields[1].getText());
+									int check = VokabeltrainerDB.hinzufuegenKarte(GUI_Main.this.chosenKartei.getNummer(), k);
+									if (check == 0) {
+										JOptionPane.showMessageDialog(GUI_Main.this, "Wort erfolgreich hinzugefügt", "Neues Wort",
+												JOptionPane.INFORMATION_MESSAGE);
+										newWord.dispose();
+									} else {
+										JOptionPane.showMessageDialog(newWord, "Ein Fehler ist aufgetreten!", "Fehler bei hinzufügen",
+												JOptionPane.ERROR_MESSAGE);
+									}
+								} else {
+									JOptionPane.showMessageDialog(newWord, "Keine Wörter eingegeben!", "Fehler bei Eingabe",
+											JOptionPane.ERROR_MESSAGE);
+								}
+							}
+
+						}
+					});
+					newWord.btn_cancel.addMouseListener(new MouseAdapter() {
+
+						@Override
+						public void mouseClicked(MouseEvent e) {
+							newWord.dispose();
+						}
+
+					});
+				}
+
+			}
+		});
+
+		tab.eingabe.addKeyListener(new KeyAdapter() {
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					checkEntry();
+				}
+			}
+
+		});
+
+		tab.btnOk.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				if (e.getButton() == 1) {
+					checkEntry();
+				}
+
+			}
+		});
+
+		tab.btnSwitch.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				if (e.getButton() == 1) {
+					VokabeltrainerDB.exportierenKarten(GUI_Main.this.chosenKartei.getNummer(),
+							"Lernkarteien/" + GUI_Main.this.chosenKartei.getBeschreibung() + ".txt", true);
+					VokabeltrainerDB.aendernLernkartei(new Lernkartei(GUI_Main.this.chosenKartei.getNummer(),
+							GUI_Main.this.chosenKartei.getBeschreibung(), GUI_Main.this.chosenKartei.getWortEinsBeschreibung(),
+							GUI_Main.this.chosenKartei.getWortZweiBeschreibung(), !GUI_Main.this.chosenKartei.getRichtung(),
+							GUI_Main.this.chosenKartei.getGrossKleinschreibung()));
+					VokabeltrainerDB.loeschenAlleFaecher(GUI_Main.this.chosenKartei.getNummer());
+					importKarten();
+					updateCenterDialog(Color.BLACK, "Geben Sie Ihre Antwort ein: ");
+					createRandomKarte("In diesem Fach sind keine weiteren Wörter");
+					GUI_Main.this.tabs.get(GUI_Main.this.tabbedPane.getSelectedIndex()).eingabe.setText("");
+				}
+
+			}
+		});
+
+	}
+	
 }
